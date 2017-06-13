35dadf634acab0d4d75076e3b4e8eb586c4673ce
vikvnsmp <vikvnsmp@googlemail.com>
2017-06-12 23:20:40 -0700

    First commit..

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	private Logger logger = LoggerFactory.getLogger(MyController.class);
+	
+	@RequestMapping("/")
+	public String test(){
+		return "Hello";
+	}
+
+}
