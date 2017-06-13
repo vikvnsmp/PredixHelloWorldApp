8a1b8f71a895acf28b495bb78e52eaf503e7714e
vikvnsmp <vikvnsmp@googlemail.com>
2017-06-12 23:27:53 -0700

    test commit..

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class MyController { 
 	
 	private Logger logger = LoggerFactory.getLogger(MyController.class);
 	
